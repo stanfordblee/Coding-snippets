94eaf72e97fa50641e5e9d8a88044b90cd282101
Stanford Lee <stanfordblee@gmail.com>
2021-01-06 15:44:41 +1100

    Improved functionality and text narrative

--- RPG Bossfight/bossfight.c
+++ RPG Bossfight/bossfight.c
@@ -1,5 +1,5 @@
 // A simulation of 3 heroes fighting a boss in an RPG
-// Created on 01/08/2020
+// Created on 01/08/2020 by Stanford Lee
 //
 // User inputs a 'lucky number', and then simulation is executed
 // Heroes and boss attack simultaneously - if hero is defeated, switch to next hero
@@ -21,76 +21,63 @@ struct character {
 	int health;
 };
 
+void create_chars(struct character *boss, struct character hero[TEAM_SIZE]);
 int introduction(void);
-
 int randomise(void);
-
 int still_alive(struct character *boss, struct character hero[TEAM_SIZE]);
-
 void attack(struct character *attacker, struct character *defender);
-
+void battle(struct character *boss, struct character hero[TEAM_SIZE]);
 void end_fight(struct character *boss);
 
 int main (void) {
+
+    struct character hero[TEAM_SIZE];
+    struct character boss;
+
+    // Create individual characters
+    create_chars(&boss, hero);
 	
+    // Provide text introduction to the game
+    introduction();
+
+	// Entering a lucky number to decide srand, which impacts damage dealt
+	srand(randomise());
+	
+	// While at least 1 hero and boss are both still alive, continue fighting
+	battle(&boss, hero);
+
+	// End of fight, print victory/defeat message
+	end_fight(&boss);
+
+	return 0;
+}
+
+// Create structs for heroes and enemy boss character
+void create_chars(struct character *boss, struct character hero[TEAM_SIZE]){
 	// Hero character array
-	struct character hero[TEAM_SIZE];
-	
 	// For hero 1 of 3: Knight
 	strcpy(hero[0].class, "Knight");
 	strcpy(hero[0].ability, "Gigas Wave");
 	hero[0].power = 8;
 	hero[0].health = 20;
-	
 	// For hero 2 of 3: Mage
 	strcpy(hero[1].class, "Mage");
 	strcpy(hero[1].ability, "Ice Barrage");
 	hero[1].power = 17;
 	hero[1].health = 8;
-	
 	// For hero 3 of 3: Archer
 	strcpy(hero[2].class, "Archer");
 	strcpy(hero[2].ability, "Hurricane Shot");
 	hero[2].power = 12;
 	hero[2].health = 12;
-	
 	// Boss character: King Black Dragon
-	struct character boss;
-	
-	strcpy(boss.class, "King Black Dragon");
-	strcpy(boss.ability, "Fire Beam");
-	boss.power = 25;
-	boss.health = 30;
-	
-    introduction();
-
-	// Entering a lucky number to decide srand
-	srand(randomise());
-	
-	// While at least 1 hero and boss are both still alive, continue fighting
-	int current_hero = 0;
-	int round_counter = 1;
-	while (still_alive(&boss, hero)) {
-		// Cycle through to next hero if current one is defeated
-		if (hero[current_hero].health <= 0) {
-			current_hero++;
-		}
-		
-		// Simultaneous attack!
-		printf("*** Round %d! ***\n", round_counter);
-		attack(&hero[current_hero], &boss);
-		attack(&boss, &hero[current_hero]);
-		printf("\n");
-		
-		round_counter++;
-	}
-	
-	// End of fight
-	end_fight(&boss);
-	
-	return 0;
+	strcpy(boss->class, "King Black Dragon");
+	strcpy(boss->ability, "Fire Beam");
+	boss->power = 25;
+	boss->health = 30;
 }
 
+// Text-based narrative introduction to the game
 int introduction(void){
     printf("\nThe band of adventurers finally reach the end of the lava maze and are faced with an ashen expanse.\n");
     printf("They hear a thunderous roar that trembles the charred ground beneath them. They steel themselves for the final battle.\n");
@@ -105,7 +92,7 @@ int randomise(void){
 	int lucky_number = 0;
 	int looper = 1;
 	while (looper) {
-		printf("\nWhich symbol was the right one?\n");
+		printf("\nWhich symbol should the adventurers choose?\n");
         printf("Enter a number between 1 to 8: ");
 		scanf("%d", &lucky_number);
 		if (lucky_number >= LUCKY_NUMBER_START && lucky_number <= LUCKY_NUMBER_END) {
@@ -157,14 +144,35 @@ void attack(struct character *attacker, struct character *defender) {
 	}
 }
 
+void battle(struct character *boss, struct character hero[TEAM_SIZE]){
+    int current_hero = 0;
+	int round_counter = 1;
+	while (still_alive(boss, hero)) {
+		// Cycle through to next hero if current one is defeated
+		if (hero[current_hero].health <= 0) {
+			current_hero++;
+		}
+		
+		// Simultaneous attack!
+		printf("*** Round %d! ***\n", round_counter);
+		attack(&hero[current_hero], boss);
+		attack(boss, &hero[current_hero]);
+		printf("\n");
+		
+		round_counter++;
+	}
+}
+
 // End of fight
 void end_fight(struct character *boss) {
 	printf("The battle has ended!\n\n");
 	if (boss->health <= 0) {
+        printf("Imbued with the power of the ancient spirits, the heroes resist defeat and vanquish their foe!\n");
         printf("The magic symbol the heroes selected was the right one!\n");
-		printf("The heroes are victorious! King Black Dragon has been defeated!\n");
+		printf("\nThe heroes are victorious! King Black Dragon has been defeated, and his legend is put to rest forever!\n");
 	} else {
-        printf("The magic symbol the heroes selected wasn't the right one!\n");
-		printf("The heroes have been defeated. King Black Dragon triumphs today!\n");
+        printf("Unfortunately, the magic symbol the heroes selected was ineffective!\n");
+        printf("Without the ancient spell, the heroes faced an impossible opponent.\n");
+		printf("\nThe heroes have been defeated and have failed in their quest. King Black Dragon triumphs today!\n");
 	}
 }
